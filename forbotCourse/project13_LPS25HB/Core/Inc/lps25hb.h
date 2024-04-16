876b52a2b4c82c60f2f7cd534f0b37af692b2267
JaKaczocha <jakub.kaczocha44295@gmail.com>
2024-04-16 23:16:58 +0200

    temp and pressure

---
+++ forbotCourse/project13_LPS25HB/Core/Inc/lps25hb.h
@@ -0,0 +1,34 @@
+/*
+ * lps25hb.h
+ *
+ *  Created on: Apr 16, 2024
+ *      Author: User
+ */
+
+#ifndef INC_LPS25HB_H_
+#define INC_LPS25HB_H_
+
+#include "stm32l4xx.h"
+
+// Inicjalizacja czujnika LPS25HB
+// Obudzenie i automatyczne pomiary
+// return - HAL_OK lub HAL_ERROR
+HAL_StatusTypeDef lps25hb_init(void);
+
+// Odczyt temperatury
+// return - wynik w stopniach C
+float lps25hb_read_temp(void);
+
+// Odczyt ciśnienia
+// reuturn - wynik w hPa
+float lps25hb_read_pressure(void);
+
+// Kalibracja czujnika ciśnienia
+// value - 16-bitowa korekcja pomiaru
+void lps25hb_set_calib(uint16_t value);
+
+// Kalibracja czujnika ciśnienia
+// value - 16-bitowa korekcja pomiaru
+void lps25hb_set_calib(uint16_t value);
+
+#endif /* INC_LPS25HB_H_ */
